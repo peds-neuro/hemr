f3f9e42c17629d98e7c6521a7a92b0e64ad78aa8
Prelang Builder <builder@prelang.com>
2015-06-20 18:47:14 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Document' to allow for create, update

--- app/admin/document.rb
+++ app/admin/document.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Document do
+  permit_params :source_author, :source_institution, :department, :type, :title, :body, :structured_data, :default_view, :source_file_filename, :source_file_content_type, :source_file_file_size, :source_file_updated_at
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
